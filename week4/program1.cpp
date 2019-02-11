6db00c22a0af7c9b9d81ed4592d49d957df4eeb3
Chris <chrismorrisrva@gmail.com>
2019-02-10 21:43:29 -0500

    Calc mean and SD.

--- week4/program1.cpp
+++ week4/program1.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <math.h>
+#include <cmath>
 using namespace std;
 
+
 int getUserInput();
-//double calcMean();
-//double calcSD();
+double calcMean(int numbers[]);
+double calcSD(int numbers[], double mean);
 
 int getUserInput(){
     int value;
@@ -12,10 +15,35 @@ int getUserInput(){
     return value;
 }
 
+double calcMean(int numbers[]){
+    int sum = 0;
+    double mean;
+    for(int i = 0; i < 10; i++)
+    {
+      sum += numbers[i]; 
+    }
+    mean = double(sum)/10;
+    return mean;
+}
+double calcSD(int numbers[], double mean){
+    double sigma=0;
+    double standevn;
+    //double value1;
+    for(int i = 0; i < 10; i++)
+    {
+        //value1 = numbers[i]-mean;
+        sigma += pow(numbers[i]-mean, 2);
+    }
+    standevn  = sqrt(sigma/10);
+}
+
+
 
 
 int main(){
     int NumberArray[10];
+    double mean;
+    double standev;
     for(int i = 0; i < 10; i++)
     {
         NumberArray[i]=getUserInput();
@@ -25,6 +53,14 @@ int main(){
     {
         cout << j+1 <<": "<< NumberArray[j] << endl;
     }
+
+    mean = calcMean(NumberArray);
+    cout << "The mean value is: " << mean << endl; 
+
+    standev = calcSD(NumberArray, mean);
+    cout << "The standard deviation is: " << standev << endl; 
+
+
     return 0;
 }
 
