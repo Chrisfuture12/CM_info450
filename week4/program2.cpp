abe73a08410d7bfa93a42ba32a3c4bf878ec88e3
Chris <chrismorrisrva@gmail.com>
2019-02-11 00:47:47 -0500

    Full pg2

--- week4/program2.cpp
+++ week4/program2.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-
 int main(){
 
+    string nameArray[5][5] = {
+                        {"Sequan","Chris","Mohmed","madiha","hunter"},
+                        {"joshua","adam","Andrew","Alexandra","Patrick"},
+                        {"Rudy","Jenish","Shaswat","Hafsa","Glenn"},
+                        {"Arthur","Stone","Trang","Katelyn","Allen"},
+                        {"Marquis","Omar","Zeeshan","Omar","Zeeshan"}    
+    };
+    cout << "Please press enter for class list." << endl;
+    if (cin.get() == '\n')
+    {
+        int row; 
+        int col;
+        for (row = 0;row < 5;row++){
+            for (col = 0;col < 5; col++){
+                cout << nameArray[row][col] << endl;
+            }
+        } 
+    } else {
+        "You did not follow the command. The End.";
+    }
     return 0;
 }
